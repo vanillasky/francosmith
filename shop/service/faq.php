7e938a063920595ade5654bf369d877a8652fe57
shkim <vanillasky9@gmail.com>
2017-07-28 09:28:18 +0900

    17-07-27 패치적용 - FAQ 한글 검색 오류

--- shop/service/faq.php
+++ shop/service/faq.php
@@ -3,6 +3,26 @@
 include "../_header.php";
 include "../lib/page.class.php";
 
+//부정태그사용방지
+if (class_exists('validation')) {
+	$validation = new validation();
+	if (method_exists($validation, 'check_digit')) {
+		if ($_GET['ssno'] != null && $validation->check_digit($_GET['ssno']) === false) {
+			$_GET['ssno'] = '';
+		}
+		if ($_GET['sitemcd'] != null && $validation->check_digit($_GET['sitemcd']) === false) {
+			$_GET['sitemcd'] = '';
+		}
+	}
+	if (method_exists($validation, 'xssCleanArray')) {
+		$_GET = $validation->xssCleanArray($_GET, array(
+				Validation::DEFAULT_KEY => 'text',
+				'faq_sword' => 'html',
+				'sword' => 'html'
+		));
+	}
+}
+
 $codeitem = codeitem('faq');
 
 # 압축코드 정의
@@ -28,9 +48,11 @@ $summary_replace[] = $cfg["compPhone"];			# 쇼핑몰 전화
 $summary_replace[] = $cfg["compFax"];			# 쇼핑몰 팩스
 $summary_replace[] = $cfg["adminEmail"];		# 쇼핑몰 이메일
 
-if ( isset( $_GET[ssno] ) == false && isset( $_GET[sitemcd] ) == false && isset( $_GET[faq_sword] ) == false ){
+$sword_flag = isset( $_GET['sword'] ) || isset( $_GET['faq_sword'] );
+if ( isset( $_GET[ssno] ) == false && isset( $_GET[sitemcd] ) == false && $sword_flag == false ){
 	$_GET['sitemcd'] = array_shift( array_keys( $codeitem ) ); // 분류 기본값
 }
+
 
 ### FAQ
 $pg = new Page($_GET[page],1000);
@@ -45,7 +67,10 @@ if ($_GET[sitemcd]){
 	$where[] = "itemcd='$_GET[sitemcd]'";
 }
 
-if ($_GET[faq_sword]){
+if ($_GET[sword]){
+	$where[] = "concat(question, descant, answer) like '%$_GET[sword]%'";
+}
+else if ($_GET[faq_sword]){
 	$where[] = "concat(question, descant, answer) like '%$_GET[faq_sword]%'";
 }
 
